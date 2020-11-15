766fe876509852f909d72d90084a8c56cc1d2335
Joe Askharoun <faielgila@gmail.com>
2020-11-14 18:16:09 -0800

    Vector and VectorFunc classes

---
+++ ProjectEstrada.Core/DimensionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectEstrada.Core
+{
+	public class DimensionException : Exception
+	{
+		public override string Message { get; }
+
+		public DimensionException() { }
+
+		public DimensionException(string message)
+		{
+			Message = message;
+		}
+	}
+}
---
+++ ProjectEstrada.Core/VectorFunc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEstrada.Core
+{
+	public class VectorFunc
+	{
+		Func<double, Vector> vectorFunc;
+
+		public int Dimension { get; internal set; }
+
+		public VectorFunc(params Func<double, double>[] functions)
+		{
+			vectorFunc = (double t) =>
+			{
+				Vector r = new Vector(functions.Length);
+				for (int i = 0; i < r.Dimension; i++)
+				{
+					r.SetComponent(i, functions[i](t));
+				}
+				return r;
+			};
+
+			Dimension = functions.Length;
+		}
+
+		public VectorFunc(Func<double, Vector> function, int dim)
+		{
+			vectorFunc = function;
+			Dimension = dim;
+		}
+
+		public Vector Evaluate(double t)
+		{
+			return vectorFunc(t);
+		}
+	}
+}
